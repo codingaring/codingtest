bf93517e1fa0116c68338c28ac14f88064bdc9b0
codingaring <sksms2104@naver.com>
2024-12-17 16:18:22 +0900

    5-1: 두 포인터 사용하여 풀이

--- 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/03. 연속부분수열1/김유경/index.js
+++ 05. 효율성(투포인터 알고리즘, 슬라이디윈도우, 해쉬)/03. 연속부분수열1/김유경/index.js
@@ -1,8 +1,23 @@
-function solution(m, arr) {
-  let answer = 0;
+function solution(N, M, arr) {
+  let count = 0;
+  let sum = 0;
+  let j = 0;
 
-  return answer;
+  for (let i = 0; i < N; i++) {
+    sum += arr[i]; // 윈도우에 현재 값을 추가
+
+    while (sum > M) {
+      sum -= arr[j]; // 윈도우 크기 조정
+      j++;
+    }
+
+    if (sum === M) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 let a = [1, 2, 1, 3, 1, 1, 1, 2];
-console.log(solution(6, a));
+console.log(solution(8, 6, a));
